c514dc6eb2d5371cc17f59847f028db0a9bde340
Sri Charan Dutta <cherrysri1997@gmail.com>
2021-08-23 20:33:25 +0530

    Committing whole project.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
