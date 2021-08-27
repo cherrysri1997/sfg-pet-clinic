e20265a80a495ad791e470ef0183ab5ab43a0b5f
Sri Charan Dutta Siram <cherrysri1997@gmail.com>
2021-08-27 11:45:14 +0530

    Separate controllers classes for Vets and Owners.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class OwnerController {
 
-    @RequestMapping({"", "/", "index", "index.html"})
-    public String getIndexPage() {
-        return "index";
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String getOwnersIndexPage() {
+        return "owners/index";
     }
 
 }
