f368297d8d6ebce661b6dc899705cfb44921f788
Sri Charan Dutta Siram <cherrysri1997@gmail.com>
2021-09-05 15:52:03 +0530

    Refactoring code. No important changes.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -2,8 +2,6 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet, Long> {
 //    These methods are commented or removed because they are already declared in a base interface called CrudService
 //
