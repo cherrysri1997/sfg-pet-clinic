4177d5dad1fb392260be91de6f0735501c788e62
Sri Charan Dutta Siram <cherrysri1997@gmail.com>
2021-09-21 13:38:19 +0530

    Implemented BaseEntity (JPA). Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,10 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private static final long serialVersionUID = 43L;
 
     public Long getId() {
         return id;
