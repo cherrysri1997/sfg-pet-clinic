9dbda71b3d434621a4b2b981ed45296149a2eb63
Sri Charan Dutta Siram <cherrysri1997@gmail.com>
2021-09-22 11:10:08 +0530

    Implemented VisitMapService. Closes #45

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
@@ -2,12 +2,21 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
 class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
+
+    private final VisitService visitService;
+
+    PetMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
@@ -25,6 +34,16 @@ class PetMapService extends AbstractMapService<Pet, Long> implements PetService 
 
     @Override
     public Pet save(Pet object) {
+        if(object.getVisits() == null) {
+            object.setVisits(new HashSet<>());
+        } else {
+            object.getVisits().forEach(visit -> {
+                if(visit.getId() == null) {
+                    visit.setPet(object);
+                    visitService.save(visit);
+                }
+            });
+        }
         return super.save(object);
     }
 
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,47 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final PetService petService;
+
+    public VisitMapService(PetService petService) {
+        this.petService = petService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if(object.getPet() != null && object.getPet().getId() == null) {
+            object.getPet().getVisits().add(object);
+            petService.save(object.getPet());
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
